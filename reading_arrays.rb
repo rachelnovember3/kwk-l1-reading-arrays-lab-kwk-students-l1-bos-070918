1c73951dfc49d636901c0f25b2b7fcf183124e20
rachelnovember3 <rachelnovember3@gmail.com>
2018-07-11 14:33:30 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -15,11 +15,13 @@ end
 
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
+  STUDENT_NAMES[4]
   
 end
 
 def last_student_by_index
   # Write a solution that returns the first student in the array student_names
+  
 end
 
 def first_student_by_method
