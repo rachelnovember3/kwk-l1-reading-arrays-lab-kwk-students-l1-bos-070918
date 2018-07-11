ffc31cba3c4f062cbd76aade4ad185c351a8f4a8
rachelnovember3 <rachelnovember3@gmail.com>
2018-07-11 14:34:56 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -28,12 +28,17 @@ end
 
 def first_student_by_method
   # Write a solution that returns the first student using the built in .first method
+  STUDENT_NAMES.first
+  
 end
 
 def last_student_by_method
   # Write a solution that returns the first student using the built in .last method
+  STUDENT_NAMES.last
+  
 end
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
+  
 end
